e8eca96443fa2d6b18a8b2f62be231fc0b4bd616
Doug Crescenzi <doug@telapi.com>
2013-01-10 07:16:27 -0500

    added new test using RSpec

--- spec/cuse_spec.rb
+++ spec/cuse_spec.rb
@@ -1,8 +1,24 @@
-# cuse_spec.rb
 require 'cuse'
 
-describe "The following tests the 'puts_with_append' method."
-def puts_test
-	# Coming soon once I learn how to use RSpec :)	
-	
+describe Kernel do
+  describe "#puts (overridden)" do
+    context "with one argument" do
+      it "should append the appropriate string" do
+        $stdout.stub!(:write)
+        $stdout.should_receive(:write).with("One Let's Go Orange!")
+        puts "One"
+      end
+    end
+
+    context "with more then one argument" do
+      it "should append the appropriate string to every arg" do
+        $stdout.stub!(:write)
+        $stdout.should_receive(:write).with("One Let's Go Orange!")
+        $stdout.should_receive(:write).with("Two Let's Go Orange!")
+        puts("One", "Two")
+      end
+    end
+  end
 end
+
+
